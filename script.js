5c60e0c6a81c96fee4e3306577d4829069602117
Dizzizz <134992026+Dizzizz@users.noreply.github.com>
2023-05-29 22:08:44 -0600

    Update script.js
    added the logic for the manager button, where the click count is checked against the manager price before making the purchase. If the player has enough clicks, the manager count is increased, the manager price is doubled, and the clicks per second are updated accordingly.

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let factoryCount = 0;
 let upgradeCount = 0;
 let clicksPerSecond = 0;
 
+let workerPrice = 10;
+let managerPrice = 300;
+let factoryPrice = 700;
+let upgradePrice = 100;
+
 // Click button event listener
 document.getElementById("clickButton").addEventListener("click", function() {
   clickCount += 0.1 * Math.pow(2, upgradeCount);
@@ -27,8 +32,20 @@ document.getElementById("buyWorkerButton").addEventListener("click", function() 
 
 // Manager button event listener (similar logic for factory and upgrade buttons)
 document.getElementById("buyManagerButton").addEventListener("click", function() {
-  // Logic for buying manager
+  if (clickCount >= managerPrice) {
+    clickCount -= managerPrice;
+    managerCount++;
+    managerPrice *= 2;
+    clicksPerSecond += 0.3;
+    updateClickCount();
+    updateManager();
+    updateCPS();
+  }
 });
+
+// Factory button event listener
+
+// Upgrade button event listener
 
 // Function to update click count display
 function updateClickCount() {
@@ -40,6 +57,8 @@ function updateWorker() {
   document.getElementById("workerPrice").textContent = workerPrice;
   document.getElementById("workerProduction").textContent = (0.1 * workerCount).toFixed(1);
 }
+
+// Function to update manager display
 
 // Function to update clicks per second display
 function updateCPS() {
